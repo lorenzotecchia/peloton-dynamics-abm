b59de17ba08945db9d5ad9334a9a8c49399dfe05
Tai Ho (tai271828) <e@tai271828.me>
2026-06-26 08:19:09 +0200

    SA: upload SA data. watch current. plotting script entrypoint: scripts/plot-SA.sh

---
+++ scripts/plot-SA.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+# current one, will be used for presentation and report until "sequential data" is ready
+uv run python scripts/plot_gsa_morris.py    --in ./data-SA/24216220-4c3f619-morris-parallel/gsa_morris.csv   --out ./data-SA/24216220-4c3f619-morris-parallel.png
+uv run python scripts/plot_gsa_sobol.py     --in ./data-SA/24216291-4c3f619-sobol-parallel/gsa_sobol.csv     --out ./data-SA/24216291-4c3f619-sobol-parallel.png
+uv run python scripts/plot_gsa_sobol_s2.py  --in ./data-SA/24216291-4c3f619-sobol-parallel/gsa_sobol_S2.csv  --out ./data-SA/24216291-4c3f619-sobol-S2-parallel.png
+
+# the below are reference to ensure the parallel data are correct
+uv run python scripts/plot_gsa_morris.py    --in ./data-SA/24217522-11cc007-morris-128sample/gsa_morris.csv  --out ./data-SA/24217522-11cc007-morris-128sample.png
+uv run python scripts/plot_gsa_sobol.py     --in ./data-SA/24217056-11cc007-sobol-128sample/gsa_sobol.csv    --out ./data-SA/24217056-11cc007-sobol-128sample.png
+uv run python scripts/plot_gsa_sobol_s2.py  --in ./data-SA/24217056-11cc007-sobol-128sample/gsa_sobol_S2.csv --out ./data-SA/24217056-11cc007-sobol-S2-128sample.png
+
+uv run python scripts/plot_gsa_morris.py    --in ./data-SA/24214965-6896023-morris-parallel/gsa_morris.csv   --out ./data-SA/24214965-6896023-morris-parallel.png
+uv run python scripts/plot_gsa_sobol.py     --in ./data-SA/24214967-6896023-sobol-parallel/gsa_sobol.csv     --out ./data-SA/24214967-6896023-sobol-parallel.png
+#uv run python scripts/plot_gsa_sobol_s2.py  --in ./data-SA/24214967-6896023-sobol-parallel/gsa_sobol_S2.csv  --out ./data-SA/24214967-6896023-sobol-S2-parallel.png
+
